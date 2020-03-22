615c8839d49b8b175b3f3ca9cc69328aa325dae6
Robin Tegg <robin@tegg.me.uk>
2020-03-22 11:48:39 +0000

    Initial commit

---
+++ src/main/java/com/robintegg/turbo/TurbolinksStimulusSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.robintegg.turbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurbolinksStimulusSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurbolinksStimulusSpringBootDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/robintegg/turbo/TurbolinksStimulusSpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robintegg.turbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurbolinksStimulusSpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
